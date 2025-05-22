0b12b31bde023779fc27acc9e189af964f4ccc49
alanbrunoscience <alanbrunomath@gmail.com>
2025-05-22 18:40:20 -0300

    :sparkles: Update the 'app.controller.ts' class

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('home')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  // localhost:3000/home/hello
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
   }
